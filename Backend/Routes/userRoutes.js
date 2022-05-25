4416bade5ad182895e14132a3afe9a8bd585d801
Criztian Jade <crizchandev@gmail.com>
2022-05-26 06:20:28 +0800

    added Protected Routes

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,10 +4,11 @@ const {
   userLogin,
   userData,
 } = require("../Controller/userController");
+const protect = require("../Middleware/authToken");
 const router = express.Router();
 
 router.post("/register", userRegister);
 router.post("/login", userLogin);
-router.get("/me", userData);
+router.get("/me", protect, userData);
 
 module.exports = router;
