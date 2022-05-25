71d97f101e859816960b8d9837fd93103f0cfff7
Criztian Jade <crizchandev@gmail.com>
2022-05-26 05:13:54 +0800

    Initial Server Setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`Server is Running on PORT:${PORT}`));
