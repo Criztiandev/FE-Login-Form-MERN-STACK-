d3dbbcb25a072c3c8ae24c687370811d95013bc0
Criztian Jade <crizchandev@gmail.com>
2022-05-26 05:33:23 +0800

    added Database Connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./Config/db");
 const errorHandler = require("./Middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
